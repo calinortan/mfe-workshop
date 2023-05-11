2c71583643aa82921b5e7e579b9fcd2a4bbcf9e0
Calin Ortan <calinortan@gmail.com>
2023-05-11 12:30:53 +0300

    working

--- babbel-host-app/vite.config.ts
+++ babbel-host-app/vite.config.ts
@@ -1,12 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     minify: false,
+    target: "esnext",
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "host-app",
+      remotes: {
+        remote_app: {
+          external: `Promise.resolve(window.microfrontendUrl)`,
+          externalType: "promise",
+        },
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
   preview: {
     port: 3456,
   },
--- babbel-remote-app/vite.config.ts
+++ babbel-remote-app/vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     minify: false,
+    target: "esnext",
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "remdasdasd",
+      filename: "remoteEntry.js",
+      // Modules to expose
+      exposes: {
+        "./Button": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
   preview: {
     port: 4567,
   },
