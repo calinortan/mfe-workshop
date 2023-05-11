bc9bd16207572119ecab676321aedcad4c16ce75
Calin Ortan <calinortan@gmail.com>
2023-05-11 08:48:52 +0300

    initial setup

---
+++ babbel-host-app/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: false,
+  },
+  plugins: [react()],
+  preview: {
+    port: 3456,
+  },
+});
